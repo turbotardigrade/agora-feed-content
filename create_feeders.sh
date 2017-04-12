0951c86957cca7d4eec40eaa805beb655566ccbb
Ubuntu <ubuntu@ip-172-31-30-34.ap-northeast-1.compute.internal>
2017-04-12 14:24:48 +0000

    Fix feeder creation script

--- create_feeders.sh
+++ create_feeders.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+NUM=$1
+PW=$2
+
 if [ $# -eq 0 ]; then
     echo "Use like this:"
     echo "\t./create_feeders.sh [feederID 01-08, 18-26] [zip archieve password]"
     exit
 fi
 
-NUM=$1
-PW=$2
+echo "Creating feeder${NUM}"
 
 npm i
 
@@ -15,11 +17,16 @@ mkdir -p feeder$NUM/clean_node_data
 cp -r agora.js clean_labels.json feed-content.js node_modules peerbackend package.json feeder$NUM
 
 cd clean_node_data
-unzip -o -P $PW -p data_node$NUM.zip > data.json || echo "Error on unzipping. Right PW provided?" && exit
+unzip -o -P $PW -p data_node$NUM.zip > data.json
+if [ $? -ne 0 ]; then
+    echo "Error on unzipping. Right PW provided?"
+fi
 cp data.json ../feeder$NUM/clean_node_data/data_node01.json
 
 cd ..
 cp models/post01.pkl feeder$NUM/peerbackend/dist/post.pkl
-feeder$NUM/peerbackend/peerbackend --init
+
+cd feeder$NUM
+./peerbackend/peerbackend --init
 
 echo "feeder${NUM} created"

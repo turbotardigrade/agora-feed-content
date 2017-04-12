9c7a42f59ef7a0a51ed2dd3ab7e21d0e0555b387
Hauton Tsang <hautonjt@gmail.com>
2017-04-12 13:37:00 +0800

    fix labels, check if hash exists in result

--- feed-content.js
+++ feed-content.js
@@ -2,15 +2,17 @@ const agora = require("./agora-backend/agora.js")
 const fs = require("fs")
 const path = require('path');
 const through2 = require('through2');
-
+const crypto = require('crypto');
 const MAX_DELAY = 0;
 const MIN_DELAY = 0;
 
 const parse = require('JSONStream')
 const results = []
 
+const labels = require('./labels.json');
+
 let i = 0;
-fs.createReadStream(path.join(__dirname, 'data.json'))
+fs.createReadStream(path.join(__dirname, 'node_data/data_node01.json'))
   .pipe(parse.parse())
   .pipe(through2({objectMode: true}, function (chunk, encoding, done) {
     var self = this;
@@ -33,6 +35,9 @@ fs.createReadStream(path.join(__dirname, 'data.json'))
         "content": e.content
       }
     }, function(result) {
+      if (labels[crypto.createHash('md5').update(e.content).digest("hex")] == null) {
+        console.log("Error: md5 not in labels");
+      }
       console.log(++i)
       console.log(result);
       if (result.error) {
